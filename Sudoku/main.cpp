107493ac86c319699fbba7f9ae7902d179d50c14
承晧 刘 <liuch9923@mails.jlu.edu.cn>
2024-09-09 14:13:49 +0800

    接入SFML静态链接

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -1,24 +1,24 @@
 // master.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include "Sudoku.h"
-#include "ConsoleIO.h"
-#include "PuzzleLoaderDAT.h"
-
-int main() {
-    // 创建控制台输入输出接口
-    ConsoleIO consoleIO;
-
-    // 创建DAT格式的PuzzleLoader
-    PuzzleLoaderDAT puzzleLoader;
-
-    // 创建Sudoku游戏实例
-    Sudoku game(&consoleIO, &puzzleLoader);
-
-    game.play();
-
-    return 0;
-}
+//#include "Sudoku.h"
+//#include "ConsoleIO.h"
+//#include "PuzzleLoaderDAT.h"
+//
+//int main() {
+//    // 创建控制台输入输出接口
+//    ConsoleIO consoleIO;
+//
+//    // 创建DAT格式的PuzzleLoader
+//    PuzzleLoaderDAT puzzleLoader;
+//
+//    // 创建Sudoku游戏实例
+//    Sudoku game(&consoleIO, &puzzleLoader);
+//
+//    game.play();
+//
+//    return 0;
+//}
 
 
 //#include <iostream>
@@ -42,3 +42,30 @@ int main() {
 //   4. 使用错误列表窗口查看错误
 //   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
 //   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+//测试SFML
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
